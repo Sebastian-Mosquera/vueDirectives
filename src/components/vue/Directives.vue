a4cae3f42392b7731d14238750ff57fff1ad537c
Sebastian-Mosquera <jsmsosquerab@ufpso.edu.co>
2024-05-10 17:14:10 -0500

    vueDirectives

---
+++ src/components/vue/Directives.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const users =["Pepe", "Ana", "Juana", "Lola"]
+
+const isChecked = ref(true)
+</script>
+<template>
+    <ul>
+        <li v-for="(user, index) in users" :key="index">{{ index }}{{ user }}</li>
+    </ul>
+    <input type="checkbox" id="check" v-model="isChecked">
+    <label for="check">Pinchame</label>
+    <div class="bg-blue-400 text-white rounded py-1 px2"
+    :class="{ 'bg-orange-500':isChecked }">
+        Este es un mensaje
+    </div>
+
+<div v-show="isChecked">
+    Este es un mensaje que podria ocultar
+</div>
+<div v-if="isChecked">
+    Este es un mensaje que podria ocultar
+</div>
+<div v-else >
+    Esto se ve porque no cumplio la condicion del if
+</div>
+</template>
